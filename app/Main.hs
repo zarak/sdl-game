c564a0b390dbd520d3f43faecc5ebb918a90ecec
Zarak <7254237+zarak@users.noreply.github.com>
2021-02-04 19:24:05 +0500

    Generate cradle

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import qualified MyLib (someFunc)
+import Linear (V4(..))
+import Control.Monad (unless)
+import SDL
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  initializeAll
+  window <- createWindow "My SDL Application" defaultWindow
+  renderer <- createRenderer window (-1) defaultRenderer
+  appLoop renderer
+  destroyWindow window
+
+appLoop :: Renderer -> IO ()
+appLoop renderer = do
+  events <- pollEvents
+  let eventIsQPress event =
+        case eventPayload event of
+          KeyboardEvent keyboardEvent ->
+            keyboardEventKeyMotion keyboardEvent == Pressed &&
+              keysymKeycode (keyboardEventKeysym keyboardEvent) == KeycodeQ
+          _ -> False
+      qPressed = any eventIsQPress events
+
+  rendererDrawColor renderer $= V4 0 0 255 255
+  clear renderer
+  present renderer
+
+  unless qPressed (appLoop renderer)
